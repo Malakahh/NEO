84e3bf80149d3553ff67e1313185c436aa87b3a0
malakahh <Malakahh@users.noreply.github.com>
2017-01-19 15:22:56 +0100

    Added indication for ios

--- bluetooth.c
+++ bluetooth.c
@@ -6,143 +6,143 @@ char directedAdvertisement = 1;
 
 unsigned long GetStoredBaud()
 {
-	int i;
-	unsigned long res = 0;
+        int i;
+        unsigned long res = 0;
 
-	for (i = 0; i < 4; i++)
-	{
-		res = res | ((unsigned long)EEPROM_Read(i) << i * 8);
-		Delay_ms(20);
-	}
+        for (i = 0; i < 4; i++)
+        {
+                res = res | ((unsigned long)EEPROM_Read(i) << i * 8);
+                Delay_ms(20);
+        }
 
-	return res;
+        return res;
 }
 
 void SetStoredBaud(unsigned long baud)
 {
-	int i;
+        int i;
 
-	for (i = 0; i < 4; i++)
-	{
-		EEPROM_Write(i, (baud & (((unsigned long)0xFF) << i * 8)) >> i * 8);
-		Delay_ms(20);
-	}
+        for (i = 0; i < 4; i++)
+        {
+                EEPROM_Write(i, (baud & (((unsigned long)0xFF) << i * 8)) >> i * 8);
+                Delay_ms(20);
+        }
 }
 
 void StartDirectedAdvertisement()
 {
-	if (directedAdvertisement == 0)
-	{
-		BTSendCommand("sr,20064000\r"); // no pin code & autoadvertise & iOS mode
-		BTReboot();
-	
-		directedAdvertisement = 1;
-	}
+        if (directedAdvertisement == 0)
+        {
+                BTSendCommand("sr,20064000\r"); // no pin code & autoadvertise & iOS mode
+                BTReboot();
+        
+                directedAdvertisement = 1;
+        }
 }
 
 void StartUndirectedAdvertisement()
 {
-	if (directedAdvertisement == 1)
-	{
-		BTSendCommand("sr,24064000\r"); //No_Direct_Advertisement & no pin code & autoadvertise & iOS mode
-		BTReboot();
+        if (directedAdvertisement == 1)
+        {
+                BTSendCommand("sr,24064000\r"); //No_Direct_Advertisement & no pin code & autoadvertise & iOS mode
+                BTReboot();
 
-		directedAdvertisement = 0;
-	}
+                directedAdvertisement = 0;
+        }
 }
 
 void SetupPrivateServices()
 {
-	BTSendCommand("SS,00000001\r"); 	//Enable private service support
-	BTSendCommand("PZ\r");			//Clear current private service and characteristics
-	BTSendCommand("PS,f4f232be5a5311e68b7786f30ca893d3\r"); //Set private service UUID to be 0xf4f232be5a5311e68b7786f30ca893d3
+        BTSendCommand("SS,00000001\r");         //Enable private service support
+        BTSendCommand("PZ\r");                        //Clear current private service and characteristics
+        BTSendCommand("PS,f4f232be5a5311e68b7786f30ca893d3\r"); //Set private service UUID to be 0xf4f232be5a5311e68b7786f30ca893d3
 
-	//Add private characteristic 0x1d4b745a5a5411e68b7786f30ca893d3 to
-	//current private service. The property of this characterstic is 0x12
-	//(readable and could notify) and has a maximum data size of 0x14 (20 bytes).
-	BTSendCommand("PC,1d4b745a5a5411e68b7786f30ca893d3,12,14\r");
+        //Add private characteristic 0x1d4b745a5a5411e68b7786f30ca893d3 to
+        //current private service. The property of this characterstic is 0x12
+        //(readable and could notify and indicate) and has a maximum data size of 0x14 (20 bytes).
+        BTSendCommand("PC,1d4b745a5a5411e68b7786f30ca893d3,32,14\r");
 
-	//Add private characteristic 0xe25328b05a5411e68b7786f30ca893d3 to
-	//current private service. The property of this cahracteristic is 0x18,
-	//(writable and could notify) and has a maximum data size of 0x14 (20 bytes).
-	BTSendCommand("PC,e25328b05a5411e68b7786f30ca893d3,18,14\r");
-}	
+        //Add private characteristic 0xe25328b05a5411e68b7786f30ca893d3 to
+        //current private service. The property of this cahracteristic is 0x18,
+        //(writable and could notify) and has a maximum data size of 0x14 (20 bytes).
+        BTSendCommand("PC,e25328b05a5411e68b7786f30ca893d3,18,14\r");
+}        
 
 void InitBT()
 {
-	unsigned long previousBaud = GetStoredBaud();
+        unsigned long previousBaud = GetStoredBaud();
 
-	if (previousBaud == BT_UART_Baud_Default || previousBaud == 0xFFFFFFFF)
-	{
-		//LATB.RB0 = 1;
+        if (previousBaud == BT_UART_Baud_Default || previousBaud == 0xFFFFFFFF)
+        {
+                //LATB.RB0 = 1;
 
-		//Setup UART interfaces - UART1 is the BT module
-		UART1_Init(BT_UART_Baud_Default);
-		Delay_ms(100);
+                //Setup UART interfaces - UART1 is the BT module
+                UART1_Init(BT_UART_Baud_Default);
+                Delay_ms(100);
 
-		//Enter BT device into CMD mode
-		BTCmdMode(1);
+                //Enter BT device into CMD mode
+                BTCmdMode(1);
 
-		//Set BT device to communicate with a baud of 2400
-		//Only relevant when UART1's baud is not 2400
-		BTSendCommand("sb,0\r");
+                //Set BT device to communicate with a baud of 2400
+                //Only relevant when UART1's baud is not 2400
+                BTSendCommand("sb,0\r");
 
-		BTReboot();
+                BTReboot();
 
-		//CMD mode pin is no longer needed at 2400 baud, so turn it off.
-		BTCmdMode(0);
+                //CMD mode pin is no longer needed at 2400 baud, so turn it off.
+                BTCmdMode(0);
 
-		//Set baud for next boot
-		SetStoredBaud(BT_UART_Baud);
-	}
+                //Set baud for next boot
+                SetStoredBaud(BT_UART_Baud);
+        }
 
-	UART1_Init(BT_UART_Baud);
-	Delay_ms(100);
+        UART1_Init(BT_UART_Baud);
+        Delay_ms(100);
 
-	//BTFactoryReset();
+        //BTFactoryReset();
 
-	BTSendCommand("s-,NEO\r");
+        BTSendCommand("s-,NEO\r");
 
-	SetupPrivateServices();
+        SetupPrivateServices();
 
-	BTReboot();
+        BTReboot();
 
-	StartUndirectedAdvertisement();	
+        StartUndirectedAdvertisement();        
 }
 
 void BTReboot()
 {
-	BTSendCommand("r,1\r");
-	Delay_ms(2000);
+        BTSendCommand("r,1\r");
+        Delay_ms(2000);
 }
 
 void BTFactoryReset()
 {
-	SetStoredBaud(0xFFFFFFFF);
-	BTSendCommand("sf,2\r");
-	Delay_ms(50);
-	BTReboot();
-	while(1);
+        SetStoredBaud(0xFFFFFFFF);
+        BTSendCommand("sf,2\r");
+        Delay_ms(50);
+        BTReboot();
+        while(1);
 }
 
 void BTCmdMode(char enter)
 {
-	if (enter)
-	{
-		#ifdef DEBUG
-			LATE.RE1 = 1;
-		#else
-			LATC.RC1 = 1;
-		#endif
-	}
-	else
-	{
-		#ifdef DEBUG
-			LATE.RE1 = 0;
-		#else
-			LATC.RC1 = 0;
-		#endif
-	}
+        if (enter)
+        {
+                #ifdef DEBUG
+                        LATE.RE1 = 1;
+                #else
+                        LATC.RC1 = 1;
+                #endif
+        }
+        else
+        {
+                #ifdef DEBUG
+                        LATE.RE1 = 0;
+                #else
+                        LATC.RC1 = 0;
+                #endif
+        }
 
-	Delay_ms(150);
+        Delay_ms(150);
 }
